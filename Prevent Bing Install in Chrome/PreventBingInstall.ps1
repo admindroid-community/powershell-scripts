57bc0b95cf5da6bbfa70a3ea3c01827b55fde64b
AdminDroid <49208841+admindroid-community@users.noreply.github.com>
2020-01-22 19:38:30 +0530

    Prevent bin installation in Chrome browser
    Office 365 ProPlus to change Chrome's default search engine to Bing in upcoming update

---
+++ Prevent Bing Install in Chrome/PreventBingInstall.ps1
@@ -0,0 +1,14 @@
+﻿$RegistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Office\16.0\Common\Officeupdate"
+
+$Name = "preventbinginstall"
+$value = "00000001"
+
+if(!(Test-Path $RegistryPath))
+{
+ Write-Host Office 365 Pro Plus not available -ForegroundColor Yellow
+}
+else
+{
+ New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+ Write-Host Successfully added registry key to prevent Bing install in Chrome -ForegroundColor Green
+}
