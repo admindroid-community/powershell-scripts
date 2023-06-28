01c6ab28166bf5ee0e95310b5446374673744d32
AdminDroid <49208841+admindroid-community@users.noreply.github.com>
2023-06-28 14:45:03 +0530

    Audit External User Activity
    Audit External User Activity

---
+++ Audit External User Activity/AuditExternalUserActivity.ps1
@@ -0,0 +1,242 @@
+﻿<#
+=============================================================================================
+Name:           Audit Office 365 external user activities report
+Description:    This script exports external user activities report to CSV
+Version:        1.0
+Website:        o365reports.com
+Script by:      O365Reports Team
+For detailed script execution: https://o365reports.com/2022/02/10/audit-office-365-external-user-activities-using-powershell
+============================================================================================
+#>
+Param
+(
+    [Parameter(Mandatory = $false)]
+    [Nullable[DateTime]]$StartDate,
+    [Nullable[DateTime]]$EndDate,
+    [string]$ExternalUserId,
+    [string]$UserName,
+    [string]$Password
+)
+
+Function Connect_Exo
+{
+ #Check for EXO v2 module inatallation
+ $Module = Get-Module ExchangeOnlineManagement -ListAvailable
+ if($Module.count -eq 0) 
+ { 
+  Write-Host Exchange Online PowerShell V2 module is not available  -ForegroundColor yellow  
+  $Confirm= Read-Host Are you sure you want to install module? [Y] Yes [N] No 
+  if($Confirm -match "[yY]") 
+  { 
+   Write-host "Installing Exchange Online PowerShell module"
+   Install-Module ExchangeOnlineManagement -Repository PSGallery -AllowClobber -Force
+  } 
+  else 
+  { 
+   Write-Host EXO V2 module is required to connect Exchange Online.Please install module using Install-Module ExchangeOnlineManagement cmdlet. 
+   Exit
+  }
+ } 
+ Write-Host Connecting to Exchange Online...
+ #Storing credential in script for scheduling purpose/ Passing credential as parameter - Authentication using non-MFA account
+ if(($UserName -ne "") -and ($Password -ne ""))
+ {
+  $SecuredPassword = ConvertTo-SecureString -AsPlainText $Password -Force
+  $Credential  = New-Object System.Management.Automation.PSCredential $UserName,$SecuredPassword
+  Connect-ExchangeOnline -Credential $Credential
+ }
+ else
+ {
+  Connect-ExchangeOnline
+ }
+}
+
+$MaxStartDate=((Get-Date).AddDays(-89)).Date
+
+#Getting external user activity for past 90 days
+if(($StartDate -eq $null) -and ($EndDate -eq $null))
+{
+ $EndDate=(Get-Date).Date
+ $StartDate=$MaxStartDate
+}
+#Getting start date to generate external user activity report
+While($true)
+{
+ if ($StartDate -eq $null)
+ {
+  $StartDate=Read-Host Enter start time for report generation '(Eg:04/28/2021)'
+ }
+ Try
+ {
+  $Date=[DateTime]$StartDate
+  if($Date -ge $MaxStartDate)
+  { 
+   break
+  }
+  else
+  {
+   Write-Host `nAudit can be retrieved only for past 90 days. Please select a date after $MaxStartDate -ForegroundColor Red
+   return
+  }
+ }
+ Catch
+ {
+  Write-Host `nNot a valid date -ForegroundColor Red
+ }
+}
+
+
+#Getting end date to audit external user activity report
+While($true)
+{
+ if ($EndDate -eq $null)
+ {
+  $EndDate=Read-Host Enter End time for report generation '(Eg: 04/28/2021)'
+ }
+ Try
+ {
+  $Date=[DateTime]$EndDate
+  if($EndDate -lt ($StartDate))
+  {
+   Write-Host End time should be later than start time -ForegroundColor Red
+   return
+  }
+  break
+ }
+ Catch
+ {
+  Write-Host `nNot a valid date -ForegroundColor Red
+ }
+}
+
+$OutputCSV=".\AuditExternalUserActivityReport_$((Get-Date -format yyyy-MMM-dd-ddd` hh-mm` tt).ToString()).csv" 
+$IntervalTimeInMinutes=1440    #$IntervalTimeInMinutes=Read-Host Enter interval time period '(in minutes)'
+$CurrentStart=$StartDate
+$CurrentEnd=$CurrentStart.AddMinutes($IntervalTimeInMinutes)
+
+#Check whether CurrentEnd exceeds EndDate
+if($CurrentEnd -gt $EndDate)
+{
+ $CurrentEnd=$EndDate
+}
+
+if($CurrentStart -eq $CurrentEnd)
+{
+ Write-Host Start and end time are same.Please enter different time range -ForegroundColor Red
+ Exit
+}
+
+Connect_EXO
+$CurrentResultCount=0
+$AggregateResultCount=0
+Write-Host `nAuditing external user activities from $StartDate to $EndDate...
+$ProcessedAuditCount=0
+$OutputEvents=0
+$ExportResult=""   
+$ExportResults=@()  
+if($ExternalUserId -eq "")
+{
+ $UserId="*#EXT*"
+}
+else
+{
+ $UserId=$ExternalUserId
+}
+
+
+while($true)
+{ 
+ #Getting external user audit data for the given time range
+ Search-UnifiedAuditLog -StartDate $CurrentStart -EndDate $CurrentEnd -UserIds $UserId -SessionId s -SessionCommand ReturnLargeSet -ResultSize 5000 | ForEach-Object {
+  $ResultCount++
+  $ProcessedAuditCount++
+  Write-Progress -Activity "`n     Retrieving external user activities from $CurrentStart to $CurrentEnd.."`n" Processed audit record count: $ProcessedAuditCount"
+  $MoreInfo=$_.auditdata
+  $Operation=$_.Operations
+  $DeletedBy=$_.UserIds
+  $AuditData=$_.auditdata | ConvertFrom-Json
+  $Workload=$AuditData.Workload
+  $PrintFlag="True"
+  $ActivityTime=(Get-Date($AuditData.CreationTime)).ToLocalTime()  #Get-Date($AuditData.CreationTime) Uncomment to view the Activity Time in UTC
+  $UserID=$AuditData.userId
+  $Operation=$AuditData.Operation
+  $ResourceType=$AuditData.ItemType
+  $ResourceURL=$AuditData.ObjectId
+  $Workload=$AuditData.Workload
+   #Export result to csv
+   $OutputEvents++
+     #Export result to csv
+  $ExportResult=@{'Activity Time'=$ActivityTime;'User Name'=$UserID;'Operation'=$Operation;'Resource URL'=$ResourceURL; 'Resource Type'=$ResourceType;'Workload'=$Workload;'More Info'=$MoreInfo}
+  $ExportResults= New-Object PSObject -Property $ExportResult  
+  $ExportResults | Select-Object 'Activity Time','User Name','Operation','Resource URL','Resource Type','Workload','More Info' | Export-Csv -Path $OutputCSV -Notype -Append 
+  
+ }
+ $currentResultCount=$currentResultCount+$ResultCount
+ 
+ if($CurrentResultCount -ge 50000)
+ {
+  Write-Host Retrieved max record for current range.Proceeding further may cause data loss or rerun the script with reduced time interval. -ForegroundColor Red
+  $Confirm=Read-Host `nAre you sure you want to continue? [Y] Yes [N] No
+  if($Confirm -match "[Y]")
+  {
+   Write-Host Proceeding audit log collection with data loss
+   [DateTime]$CurrentStart=$CurrentEnd
+   [DateTime]$CurrentEnd=$CurrentStart.AddMinutes($IntervalTimeInMinutes)
+   $CurrentResultCount=0
+   if($CurrentEnd -gt $EndDate)
+   {
+    $CurrentEnd=$EndDate
+   }
+  }
+  else
+  {
+   Write-Host Please rerun the script with reduced time interval -ForegroundColor Red
+   Exit
+  }
+ }
+
+ 
+ if($ResultCount -lt 5000)
+ { 
+  if($CurrentEnd -eq $EndDate)
+  {
+   break
+  }
+  $CurrentStart=$CurrentEnd 
+  if($CurrentStart -gt (Get-Date))
+  {
+   break
+  }
+  $CurrentEnd=$CurrentStart.AddMinutes($IntervalTimeInMinutes)
+  $CurrentResultCount=0
+  if($CurrentEnd -gt $EndDate)
+  {
+   $CurrentEnd=$EndDate
+  }
+ }                                                                                             
+ $ResultCount=0
+}
+
+#Open output file after execution
+If($OutputEvents -eq 0)
+{
+ Write-Host No records found
+}
+else
+{
+ Write-Host `nThe output file contains $OutputEvents audit records
+ if((Test-Path -Path $OutputCSV) -eq "True") 
+ {
+  Write-Host `nThe Output file availble in $OutputCSV -ForegroundColor Green
+  $Prompt = New-Object -ComObject wscript.shell   
+  $UserInput = $Prompt.popup("Do you want to open output file?",`   
+ 0,"Open Output File",4)   
+  If ($UserInput -eq 6)   
+  {   
+   Invoke-Item "$OutputCSV"   
+  } 
+ }
+}
+
+#Disconnect Exchange Online session
+Disconnect-ExchangeOnline -Confirm:$false -InformationAction Ignore -ErrorAction SilentlyContinue
