445c6776ebd3b22b2832efc7884a388af7807028
AdminDroid <49208841+admindroid-community@users.noreply.github.com>
2023-09-06 17:28:52 +0530

    Disable self-service capability in Microsoft 365
    Disable self-service capability in Microsoft 365

---
+++ Disable Self-service Purchase in M365/DisableSelfServicePurchase.ps1
@@ -0,0 +1,15 @@
+﻿<#
+=============================================================================================
+Name:              Disable self-service purchase for Microsoft 365 products
+Description:       This script disables self-service purchase/ trial capability for all the supported M365 products
+Script written by: AdminDroid community team
+Website:           admindroid.com
+=====================================================================================
+#>
+
+Install-Module -Name MSCommerce -Scope CurrentUser #once you install you should remove this line
+Import-Module -Name MSCommerce 
+Connect-MSCommerce #sign-in with your global or billing administrator account when prompted
+Get-MSCommerceProductPolicies -PolicyId AllowSelfServicePurchase | Where { $_.PolicyValue -eq “Enabled”} | forEach { 
+ Update-MSCommerceProductPolicy -PolicyId AllowSelfServicePurchase -ProductId $_.ProductID -Enabled $false  
+}
