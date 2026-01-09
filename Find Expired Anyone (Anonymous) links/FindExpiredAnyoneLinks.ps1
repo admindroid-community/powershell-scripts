901b5ffc15d07e042a2a4abd7f191f948f99e9e1
AdminDroid <49208841+admindroid-community@users.noreply.github.com>
2026-01-09 08:47:04 +0530

    Find Expired Anonymous Links

--- Find Expired Anyone (Anonymous) links/FindExpiredAnyoneLinks.ps1
+++ Find Expired Anyone (Anonymous) links/FindExpiredAnyoneLinks.ps1
@@ -1,7 +1,7 @@
 ﻿<#
 =============================================================================================
 Name:           Get All Expired Anyone Links in SharePoint Online Using PowerShell  
-Version:        1.0
+Version:        2.0
 Website:        o365reports.com
 
 ~~~~~~~~~~~~~~~~~~
@@ -18,6 +18,12 @@ Script Highlights:
 
 For detailed script execution: https://o365reports.com/2024/07/16/get-all-expired-anyone-links-in-sharepoint-online-using-powershell/
 
+~~~~~~~~~
+Change Log:
+~~~~~~~~~
+  V1.0 (Jul 16, 2024) - File created
+  V2.0 (Dec 29, 2025) - Handled ClientId requirement for SharePoint PnP PowerShell module and made minor usability changes
+
 ============================================================================================
 #>
 Param
@@ -30,23 +36,27 @@ Param
     [string]$TenantName,
     [string]$ImportCsv 
 )
+
+
 Function Installation-Module{
-    $Module = Get-InstalledModule -Name PnP.PowerShell -RequiredVersion 1.12.0 -ErrorAction SilentlyContinue
+    $Module = Get-InstalledModule -Name PnP.PowerShell -MinimumVersion 1.12.0 -ErrorAction SilentlyContinue
     If($Module -eq $null){
         Write-Host PnP PowerShell Module is not available -ForegroundColor Yellow
         $Confirm = Read-Host Are you sure you want to install module? [Yy] Yes [Nn] No
         If($Confirm -match "[yY]") { 
             Write-Host "Installing PnP PowerShell module..."
-            Install-Module PnP.PowerShell -RequiredVersion 1.12.0 -Force -AllowClobber -Scope CurrentUser
-            Import-Module -Name Pnp.Powershell -RequiredVersion 1.12.0           
+            Install-Module PnP.PowerShell -Force -AllowClobber -Scope CurrentUser
+            Import-Module -Name Pnp.Powershell           
         } 
         Else{ 
-           Write-Host PnP PowerShell module is required to connect SharePoint Online.Please install module using Install-Module PnP.PowerShell cmdlet. 
+           Write-Host "PnP PowerShell module is required to connect SharePoint Online.Please install module using 'Install-Module PnP.PowerShell' cmdlet." 
            Exit
         }
     }
     Write-Host `nConnecting to SharePoint Online...
-} 
+}
+
+
 Function Connection-Module{
     param
     (
@@ -57,7 +67,7 @@ Function Connection-Module{
     {
         $SecuredPassword = ConvertTo-SecureString -AsPlainText $Password -Force
         $Credential  = New-Object System.Management.Automation.PSCredential $AdminName,$SecuredPassword
-        Connect-PnPOnline -Url $Url -Credential $Credential
+        Connect-PnPOnline -Url $Url -ClientId $ClientId -Credential $Credential
     }
     elseif($ClientId -ne "" -and $CertificateThumbprint -ne "" -and $TenantName -ne "")
     {
@@ -65,9 +75,11 @@ Function Connection-Module{
     }
     else
     {
-        Connect-PnPOnline -Url $Url -Interactive   
+        Connect-PnPOnline -Url $Url -ClientId $ClientId -Interactive   
     }
 }
+
+
 Function Get-SharedLinkInfo($ListItems) {
     $Ctx = Get-PnPContext
     ForEach ($Item in $ListItems) {
@@ -126,51 +138,60 @@ Function Get-SharedLinkInfo($ListItems) {
         }
     }
 }
+
 Function Get-SharedLinks{
-    $ExcludedLists = @("Form Templates","Style Library","Site Assets","Site Pages", "Preservation Hold Library", "Pages", "Images",
-                       "Site Collection Documents", "Site Collection Images")
+    $ExcludedLists = @("Form Templates","Style Library","Site Assets","Site Pages", "Preservation Hold Library", "Pages", "Images", "Site Collection Documents", "Site Collection Images")
     $DocumentLibraries = Get-PnPList | Where-Object {$_.Hidden -eq $False -and $_.Title -notin $ExcludedLists -and $_.BaseType -eq "DocumentLibrary"}
     Foreach($List in $DocumentLibraries){
         $ListItems = Get-PnPListItem -List $List -PageSize 2000  | Where {$_.FileSystemObjectType -eq "File"}
         Get-SharedLinkInfo $ListItems
     }
 }
+
 Installation-Module
 $timestamp = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
-$ReportOutput = "$PSScriptRoot\Expired_SPO_Shared_Links $timestamp.csv"
+$ReportOutput = "$(Get-Location)\Expired_SPO_Anonymous_Links_$timestamp.csv"
 $Global:ItemCount = 0
-If($TenantName -eq ""){
-    $TenantName = Read-Host "Enter your tenant name (e.g., 'contoso' for 'contoso.onmicrosoft.com')"
+
+If($TenantName -eq "" -and $ImportCsv -eq "")
+{
+ $TenantName = Read-Host "Enter your tenant name (e.g., 'contoso' for 'contoso.onmicrosoft.com')"
 }
+If($ClientId -eq "")
+{
+ $ClientId= Read-Host "ClientId is required to connect PnP PowerShell. Enter ClientId"
+}
+
 
 If($ImportCsv -ne ""){
     $SiteCollections = Import-Csv -Path $ImportCsv
     Foreach($Site in $SiteCollections){
         Connection-Module -Url $Site.SiteUrl
         $Site = (Get-PnPWeb | Select Title).Title
-        Get-SharedLinks
-        Disconnect-PnPOnline 
+        Get-SharedLinks 
     }
+    Disconnect-PnPOnline -WarningAction SilentlyContinue
 }
 Else{
     Connection-Module -Url "https://$TenantName-admin.sharepoint.com"
-    $SiteCollections = Get-PnPTenantSite  | Where -Property Template -NotIn ("SRCHCEN#0", "REDIRECTSITE#0", "SPSMSITEHOST#0", "APPCATALOG#0", "POINTPUBLISHINGHUB#0", "EDISC#0", "STS#-1")
-    Disconnect-PnPOnline -WarningAction SilentlyContinue
+    $SiteCollections = Get-PnPTenantSite | Where -Property Template -NotIn ("SRCHCEN#0", "REDIRECTSITE#0", "SPSMSITEHOST#0", "APPCATALOG#0", "POINTPUBLISHINGHUB#0", "EDISC#0", "STS#-1")
     ForEach($Site in $SiteCollections)
     {
         Connection-Module -Url $Site.Url  
         $Site = (Get-PnPWeb | Select Title).Title
-        Get-SharedLinks
-        Disconnect-PnPOnline -WarningAction SilentlyContinue 
+        Get-SharedLinks 
     }
+    Disconnect-PnPOnline -WarningAction SilentlyContinue
 }
+
+
 if((Test-Path -Path $ReportOutput) -eq "True") 
 {
     Write-Host `nThe output file contains $Global:ItemCount files
     Write-Host `n The Output file availble in:  -NoNewline -ForegroundColor Yellow
     Write-Host $ReportOutput
     Write-Host `n~~ Script prepared by AdminDroid Community ~~`n -ForegroundColor Green
-    Write-Host "~~ Check out " -NoNewline -ForegroundColor Green; Write-Host "admindroid.com" -ForegroundColor Yellow -NoNewline; Write-Host " to get access to 1800+ Microsoft 365 reports. ~~" -ForegroundColor Green `n`n
+    Write-Host "~~ Check out " -NoNewline -ForegroundColor Green; Write-Host "admindroid.com" -ForegroundColor Yellow -NoNewline; Write-Host "to access 3,000+ reports and 450+ management actions across your Microsoft 365 environment. ~~" -ForegroundColor Green `n`n
     $Prompt = New-Object -ComObject wscript.shell   
     $UserInput = $Prompt.popup("Do you want to open output file?",`   
     0,"Open Output File",4)   
